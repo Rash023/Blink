00d93508a2370d74c48bf1c1a80f434398e08ec5
Rash023 <rashidmazhar0507@gmail.com>
2024-05-20 00:03:10 +0530

    Added basic frontend

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-
-## Build a basic version of PayTM
+## Blink - Make transactions with a blink
--- backend/controllers/bank.js
+++ backend/controllers/bank.js
@@ -31,44 +31,51 @@ exports.getBalance = async (req, res) => {
 //handler to transfer funds
 
 exports.transferFunds = async (req, res) => {
-  const session = await mongoose.startSession();
+  try {
+    const session = await mongoose.startSession();
 
-  session.startTransaction();
-  const { amount, to } = req.body;
-  const token = req.body.token;
-  let decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-  let id = decodedToken.userId;
-  // Fetch the accounts within the transaction
-  const account = await Bank.findOne({ userId: id }).session(session);
+    session.startTransaction();
+    const { amount, to } = req.body;
+    const token = req.body.token;
+    let decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    let id = decodedToken.userId;
+    // Fetch the accounts within the transaction
+    const account = await Bank.findOne({ userId: id }).session(session);
 
-  if (!account || account.balance < amount) {
-    await session.abortTransaction();
-    return res.status(400).json({
-      message: "Insufficient balance",
+    if (!account || account.balance < amount) {
+      await session.abortTransaction();
+      return res.status(400).json({
+        message: "Insufficient balance",
+      });
+    }
+
+    const toAccount = await Bank.findOne({ userId: to }).session(session);
+
+    if (!toAccount) {
+      await session.abortTransaction();
+      return res.status(400).json({
+        message: "Invalid account",
+      });
+    }
+
+    // Perform the transfer
+    await Bank.updateOne(
+      { userId: req.userId },
+      { $inc: { balance: -amount } }
+    ).session(session);
+    await Bank.updateOne({ userId: to }, { $inc: { balance: amount } }).session(
+      session
+    );
+
+    // Commit the transaction
+    await session.commitTransaction();
+    res.json({
+      message: "Transfer successful",
+    });
+  } catch {
+    return res.status(404).json({
+      success: false,
+      message: "Internal Server Error",
     });
   }
-
-  const toAccount = await Bank.findOne({ userId: to }).session(session);
-
-  if (!toAccount) {
-    await session.abortTransaction();
-    return res.status(400).json({
-      message: "Invalid account",
-    });
-  }
-
-  // Perform the transfer
-  await Bank.updateOne(
-    { userId: req.userId },
-    { $inc: { balance: -amount } }
-  ).session(session);
-  await Bank.updateOne({ userId: to }, { $inc: { balance: amount } }).session(
-    session
-  );
-
-  // Commit the transaction
-  await session.commitTransaction();
-  res.json({
-    message: "Transfer successful",
-  });
 };
